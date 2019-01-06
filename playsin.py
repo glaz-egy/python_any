87442369186e1961bd107fc54e6dddb2b8e5d740
Glaz Egy <rei.izayoi.0804@gmail.com>
2019-01-06 21:03:58 +0900

    更新

--- playsin.py
+++ playsin.py
@@ -4,8 +4,8 @@ import struct
 import numpy as np
 import wx
 
-def play(Hz=440, fs=8000, sec=1):
-    data = [int(15000*np.sin(2*np.pi*x*Hz/fs)) for x in range(sec*fs)]
+def play(Hz=440, fs=8000, sec=1, vol=50):
+    data = [int((15000*(vol/100))*np.sin(2*np.pi*x*Hz/fs)) for x in range(sec*fs)]
     data = struct.pack("h"*len(data), *data)
     audio = pyaudio.PyAudio()
     stream = audio.open(format=pyaudio.paInt16,
@@ -23,12 +23,12 @@ def play(Hz=440, fs=8000, sec=1):
 def loop():
     global loopFlag
     while loopFlag:
-        play(slider1.GetValue(), slider2.GetValue(), 2)
+        play(slider1.GetValue(), slider2.GetValue(), 2, slider4.GetValue())
 
 def ButtonBind(event):
     global loopFlag
     if event.GetId() == 100:
-        threading.Thread(target=play, args=(slider1.GetValue(), slider2.GetValue(), slider3.GetValue())).start()
+        threading.Thread(target=play, args=(slider1.GetValue(), slider2.GetValue(), slider3.GetValue(), slider4.GetValue())).start()
     elif event.GetId() == 101:
         loopFlag = loopb.GetValue()
         threading.Thread(target=loop).start()
@@ -37,12 +37,12 @@ def ButtonBind(event):
 if __name__=='__main__':
     loopFlag = False
     app = wx.App()
-    frame = wx.Frame(None, wx.ID_ANY, 'HSPP', size=(600, 300), style=wx.MINIMIZE_BOX | wx.CAPTION | wx.SYSTEM_MENU | wx.CLOSE_BOX)
+    frame = wx.Frame(None, wx.ID_ANY, 'HSPP', size=(600, 350), style=wx.MINIMIZE_BOX | wx.CAPTION | wx.SYSTEM_MENU | wx.CLOSE_BOX)
 
     panel = wx.Panel(frame, wx.ID_ANY)
     slidertext1 = wx.StaticText(panel, label='Hz')
     slider1 = wx.Slider(panel, style=wx.SL_HORIZONTAL|wx.SL_LABELS)
-    slider1.SetMin(100)
+    slider1.SetMin(50)
     slider1.SetMax(50000)
     slider1.SetValue(100)
     slidertext2 = wx.StaticText(panel, label='fs')
@@ -55,6 +55,11 @@ if __name__=='__main__':
     slider3.SetMin(0)
     slider3.SetMax(60)
     slider3.SetValue(0)
+    slidertext4 = wx.StaticText(panel, label='vol')
+    slider4 = wx.Slider(panel, style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+    slider4.SetMin(0)
+    slider4.SetMax(100)
+    slider4.SetValue(50)
     start = wx.Button(panel, id=100, label='start')
     loopb = wx.ToggleButton(panel, id=101, label='loop')
     layout = wx.BoxSizer(wx.VERTICAL)
@@ -64,6 +69,8 @@ if __name__=='__main__':
     layout.Add(slider2, flag=wx.GROW)
     layout.Add(slidertext3, flag=wx.GROW)
     layout.Add(slider3, flag=wx.GROW)
+    layout.Add(slidertext4, flag=wx.GROW)
+    layout.Add(slider4, flag=wx.GROW)
     layout.Add(start, flag=wx.GROW)
     layout.Add(loopb, flag=wx.GROW)
     panel.SetSizer(layout)
